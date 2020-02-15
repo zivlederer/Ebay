85c66bd3f7500e928043d0360a77ff0c54b646d4
leder <lederer.ziv@gmail.com>
2020-02-15 16:28:58 +0200

    .

---
+++ src/main/java/Base.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class Base {
+    public static WebDriver driver;
+
+    public static void init(){
+        System.setProperty("webdriver.chrome.driver", "/Users/naveenkhunteta/Downloads/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.get(System.getProperty("https://www.ebay.com/"));
+    }
+
+}
